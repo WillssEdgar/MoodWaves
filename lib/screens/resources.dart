d6940f2c08c47929082c33b199af566b264e9587
SamHUNCW <114754804+SamHUNCW@users.noreply.github.com>
2024-03-10 16:44:02 -0400

    Update resources.dart
    Added (unclickable) hyperlinks to each item in resources.dart
    
    was sick over break, so had to submit later

--- lib/screens/resources.dart
+++ lib/screens/resources.dart
@@ -1,6 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:mood_waves/classes/resource_Class.dart';
 
+
+class SearchBarApp extends StatefulWidget{
+  const SearchBarApp({super.key});
+
+  @override
+  State<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+
+class _SearchBarAppState extends State<SearchBarApp> {
+  // Add search functionality here
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = ThemeData(
+        useMaterial3: true);
+
+    return MaterialApp(
+      theme: themeData,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Search Bar Sample')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SearchAnchor(
+              builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              controller: controller,
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0)),
+              onTap: () {
+                controller.openView();
+              },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
+           
+            );
+          }, suggestionsBuilder:
+                  (BuildContext context, SearchController controller) {
+            return List<ListTile>.generate(5, (int index) {
+              final String item = 'item $index';
+              return ListTile(
+                title: Text(item),
+                onTap: () {
+                  setState(() {
+                    controller.closeView(item);
+                  });
+                },
+              );
+            });
+          }),
+        ),
+      ),
+    );
+  }
+}
+
 class ResourcesPage extends StatelessWidget {
   final List<Resource> resources = sampleLists;
 
@@ -10,15 +68,28 @@ class ResourcesPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Mental Health Resources'),
       ),
-      body: ListView.builder(
+      body: 
+      //SearchBarApp;
+      ListView.builder(
         itemCount: resources.length,
         itemBuilder: (context, index) {
           final resource = resources[index];
           return ListTile(
             title: Text(resource.resourceName),
-            subtitle: Text(resource.resourceDesc),
+            subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+          Text(resource.resourceDesc),
+          Text(resource.resourceURL,
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: const Color.fromARGB(255, 32, 104, 163),
+            ),
+          ),
+        ],
+            ),
             // text: Text(resource.resourceURL),
           );
+        
         },
       ),
     );
