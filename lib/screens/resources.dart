db72e5a8289509f981f3498f87e719c6215441d7
SamHUNCW <114754804+SamHUNCW@users.noreply.github.com>
2024-03-22 10:29:06 -0400

    Button Removal / Library Addition
    Removed an unnecessary button from the search bar in the Resources page and implemented the linkify library.

--- lib/screens/resources.dart
+++ lib/screens/resources.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mood_waves/classes/resource_Class.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+
 // import linkify library - web and email
 
 class SearchBarApp extends StatefulWidget{
@@ -12,6 +14,10 @@ class SearchBarApp extends StatefulWidget{
 
 class _SearchBarAppState extends State<SearchBarApp> {
   // Add search functionality here
+  bool loading = true;
+
+  _SearchBarAppState();
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,43 +27,16 @@ class _SearchBarAppState extends State<SearchBarApp> {
     return MaterialApp(
       theme: themeData,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Search Bar Sample')),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller) {
-            return SearchBar(
-              controller: controller,
-              padding: const MaterialStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16.0)),
-              onTap: () {
-                controller.openView();
-              },
-              onChanged: (_) {
-                controller.openView();
-              },
-              leading: const Icon(Icons.search),
-           
-            );
-          }, suggestionsBuilder:
-                  (BuildContext context, SearchController controller) {
-            return List<ListTile>.generate(5, (int index) {
-              final String item = 'item $index';
-              return ListTile(
-                title: Text(item),
-                onTap: () {
-                  setState(() {
-                    controller.closeView(item);
-                  });
-                },
-              );
-            });
-          }),
-        ),
+        appBar: AppBar(title: const Text('Search Bar Sample')), // does not display yet on screen
+        body: const Padding( padding: EdgeInsets.all(8.0)),
       ),
     );
   }
 }
+
+
+
+
 
 class ResourcesPage extends StatelessWidget {
   final List<Resource> resources = sampleLists;
@@ -66,22 +45,16 @@ class ResourcesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mental Health Resources'),
-        actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // Implement search functionality
-              },
-            ),
-          ],
+        title: const Text('Mental Health Resources'), 
+        // note to self: actions[] was here, deleted to get rid of search button
           bottom: const PreferredSize(
             preferredSize: Size.fromHeight(48.0),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
               child: TextField(
+                // onChanged: might go here, can shift order of list 
                 decoration: InputDecoration(
-                  hintText: 'Search...',
+                  hintText: 'Search...', // empty text in search bar
                   border: OutlineInputBorder(),
                 ),
               ),
@@ -89,7 +62,7 @@ class ResourcesPage extends StatelessWidget {
           ),
       ),
       body: 
-      //SearchBarApp;
+      //SearchBarApp; create a dynamic list
       ListView.builder(
         itemCount: resources.length,
         itemBuilder: (context, index) {
