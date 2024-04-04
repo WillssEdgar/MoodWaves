4c17a36d35b5b62e00c4c5ae18e2fa6c0585aed5
SamHUNCW <114754804+SamHUNCW@users.noreply.github.com>
2024-04-03 22:14:19 -0400

    Data Refinement #40
    Added new "location" String to list and display on page
    
    Began working on sorting function that puts events scheduled closer to the present first

--- lib/screens/events.dart
+++ lib/screens/events.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
 
 class EventsPage extends StatelessWidget {
+
+  /// Generates the 
+
   @override
   Widget build(BuildContext context) {
     // Dummy data for feed items
-    final List<Map<String, String>> feedItems = [
+    final List<Map<String,  String>> feedItems = [
       {
-        'title': 'Fire at Seahawk village',
-        'content': 'I would love to have a fire and get into the pool at Seahawk village',
-        'author': 'Campus Housing',
-        'date': 'Today',
+        'title': "Fire at Seahawk village",
+        'content': "Celebrate at the pool, and eat s'mores at the campfire!",
+        'author': "Campus Housing",
+        'datetime': "April 24 at 5:30PM",
+        'location': "Seahawk Village",
       },
       {
-        'title':'',
-        'content':'',
-        'author':'',
-        'date':'',
+        'title':"Spa Day",
+        'content':"Take some time to treat yourself!",
+        'author':"Seahawk Wellness",
+        'datetime': "April 26 at 2:30PM",
+        'location': "Leonard Student Recreational Center",
 
       },
       {
-        'title':'',
-        'content':'',
-        'author':'',
-        'date':'',
+        'title':"Yoga on the Beach",
+        'content':"Come join us this morning on the seaside for a calming experience!",
+        'author':"UNCW Yoga Association",
+        'datetime':"April 27 at 8:00AM",
+        'location': "Wrightsville Beach",
 
       },
       {
-        'title':'',
-        'content':'',
-        'author':'',
-        'date':'',
+        'title':"Handpan Music Festival",
+        'content':"Listen to the soothing ringing of these bell-like instruments!",
+        'author':"Blue Mountain Group",
+        'datetime':"April 28 at 12:30PM",
+        'location': "Fisher Student Center",
 
       },
     ];
@@ -37,7 +44,14 @@ class EventsPage extends StatelessWidget {
     return Scaffold(
       body: ListView.builder(
         itemCount: feedItems.length,
+        
         itemBuilder: (context, index) {
+
+            String? eventDate = feedItems[index]['datetime'];
+
+
+
+
           return Card(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -58,7 +72,8 @@ class EventsPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Text(feedItems[index]['author']!),
-                      Text(feedItems[index]['date']!),
+                      Text(feedItems[index]['location']!),
+                      Text(feedItems[index]['datetime']!),
                     ],
                   ),
                 ],
@@ -69,4 +84,12 @@ class EventsPage extends StatelessWidget {
       ),
     );
   }
+}
+
+
+/// Returns a checkable time string, allowing cards with closer time 
+/// to rise to the top
+ void _dateCreator(String eventDate) async {
+  DateTime currentDateTime =  DateTime.now(); 
+  String dateTimeString = currentDateTime.toString();
 }
