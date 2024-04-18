80efad18a3aae04b1f59328c070e4b9fc954c38e
gobannaz <156826820+anna-banna@users.noreply.github.com>
2024-04-17 20:44:40 -0400

    comments on test

--- test/details_card_test.dart
+++ test/details_card_test.dart
@@ -4,11 +4,15 @@ import 'package:mood_waves/widgets/details_card.dart';
 import 'package:mockito/mockito.dart';
 import 'mocks.mocks.dart'; // Make sure this is the correct path to the generated mocks
 
+// This test checks if the DetailsCard widget displays correctly
+// and if the appropriate information (icon, title, value) is displayed in each card
 void main() {
+  // Create mock instances
   late MockFirebaseAuth mockFirebaseAuth;
   late MockUser mockUser;
 
   setUp(() {
+    // Initialize mock instances
     mockFirebaseAuth = MockFirebaseAuth();
     mockUser = MockUser();
 
@@ -17,11 +21,12 @@ void main() {
     when(mockUser.uid).thenReturn('user123');
   });
 
+  // Test the DetailsCard widget
   testWidgets('DetailsCard displays correctly', (WidgetTester tester) async {
-    // Arrange & Act
+    // simulate the DetailsCard widget
     await tester.pumpWidget(MaterialApp(home: DetailsCard()));
 
-    // Assert
+    // Verify that the DetailsCard widget displays correctly
     expect(find.byType(DetailsCard), findsOneWidget);
   });
 }
