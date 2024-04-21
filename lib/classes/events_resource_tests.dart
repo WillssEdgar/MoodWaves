ea9c24dfb904de1009f02d3ecb86cc3d79609822
SamHUNCW <114754804+SamHUNCW@users.noreply.github.com>
2024-04-20 22:40:26 -0400

    Attempting Black-box Testing
    Strugged to configure a test setup using Mockito. Still not functioning, but code works

--- lib/classes/events_resource_tests.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
-
-
-
-
---
+++ test/events_resource_tests.dart
@@ -0,0 +1,17 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:mood_waves/classes/resource_class.dart';
+
+class mockResourceClass extends Mock implements Resource {
+
+  void main() {
+  test('mockito test', () {
+    final mock = mockResourceClass();
+    
+  });
+
+
+}
+}
